c72f39ada32b9b6b310a2a51fb0869b53608558c
h0pefu1 <h0pefu1>
2023-05-15 22:06:23 +0300

    added onion structure to the backend project.

---
+++ MFCApi/MFC.Infrastructure/DataBase/MFCDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFC.Infrastructure.DataBase
+{
+    public class MFCDbContext : DbContext
+    {
+        public MFCDbContext(DbContextOptions<MFCDbContext> options) : base(options)
+        {
+
+        }
+        public MFCDbContext()
+     : base()
+        {
+
+        }
+
+    }
+}
